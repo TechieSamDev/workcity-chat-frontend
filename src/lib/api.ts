d93d53137a12b21b67ae18c8dfc28a65bc47f6e0
Techie Sam <blessingabolade15@gmail.com>
2025-08-16 17:29:34 +0100

    chore: added url as ENV variable in socke and api instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const BASE_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000/api';
 
-// If you are using a different environment variable for production, you can adjust it accordingly.
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,7 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5000');
+
+const SOCKET_URL = import.meta.env.ENV_SOCKET_URL || 'http://localhost:5000';
+const socket = io(SOCKET_URL);
+
 console.log(socket);
 export default socket;
