d8775e080db49803d8fc5a3b74a963b7dd3e8a95
Techie Sam <blessingabolade15@gmail.com>
2025-08-16 17:12:44 +0100

    refactor: refactored component rendering for SelectRecipient componenent

--- src/features/chat/SelectRecipient.tsx
+++ src/features/chat/SelectRecipient.tsx
@@ -6,6 +6,7 @@ import useAppAuth from '@/hooks/useAppAuth';
 type SelectChatWithProps = {
   setRecipient: React.Dispatch<React.SetStateAction<Recipient | null>>;
 };
+
 type OnlineUser = {
   [id: string]: {
     name: string;
@@ -13,6 +14,7 @@ type OnlineUser = {
     socketId: string;
   };
 };
+
 const SelectChatWith = ({ setRecipient }: SelectChatWithProps) => {
   const [onlineUsers, setOnlineUsers] = useState<OnlineUser>({});
   const [userData] = useAppAuth();
@@ -24,23 +26,10 @@ const SelectChatWith = ({ setRecipient }: SelectChatWithProps) => {
     });
   }, [onlineUsers]);
 
-  const renderOnlineUser = () =>
-    Object.keys(onlineUsers)
-      ?.filter((u) => u !== userData?._id)
-      .map((id: string, i) => (
-        <div className="flex items-center border p-2 py-3 gap-2 hover:bg-gray-200 rounded">
-          <span className="font-bold">{i + 1}. </span>
-          <div
-            onClick={() => setRecipient({ ...onlineUsers[id], id })}
-            className=" w-full text-start flex justify-between"
-          >
-            <span> {onlineUsers[id]?.name} </span>-
-            <span className="bg-blue-300 text-gray-50 p-1 text-xs rounded-2xl">
-              {onlineUsers[id]?.role}
-            </span>
-          </div>
-        </div>
-      ));
+  
+  const filteredOnlineUsers = Object.keys(onlineUsers)?.filter(
+    (u) => u !== userData?._id
+  );
 
   return (
     <div>
@@ -49,7 +38,24 @@ const SelectChatWith = ({ setRecipient }: SelectChatWithProps) => {
         <div className="flex justify-between items-center bg-gray-100 p-2">
           <p>Online Users</p>
         </div>
-        <div className="space-y-3">{renderOnlineUser()}</div>
+        <div className="space-y-3">
+          {filteredOnlineUsers.length > 0
+            ? filteredOnlineUsers.map((id, i) => (
+                <div className="flex items-center border p-2 py-3 gap-2 hover:bg-gray-200 rounded">
+                  <span className="font-bold">{i + 1}. </span>
+                  <div
+                    onClick={() => setRecipient({ ...onlineUsers[id], id })}
+                    className=" w-full text-start flex justify-between"
+                  >
+                    <span> {onlineUsers[id]?.name} </span>-
+                    <span className="bg-blue-300 text-gray-50 p-1 text-xs rounded-2xl">
+                      {onlineUsers[id]?.role}
+                    </span>
+                  </div>
+                </div>
+              ))
+            : <div className='text-gray-300'>No User Online</div>}
+        </div>
       </div>
     </div>
   );
